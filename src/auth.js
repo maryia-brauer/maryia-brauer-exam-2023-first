85f6601233e2728f7c129231294d1fbe2898bde6
Maryia <maryia.brauer@gmail.com>
2023-11-02 01:03:20 +0200

    Done task 4

---
+++ src/auth.js
@@ -0,0 +1,65 @@
+import { defineStore } from 'pinia';
+import { router } from './router/index.js'; 
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: {
+      name: 'NAME',
+      surname: 'SURNAME',
+      code: 'IT1234',
+      favorite_songs: [], 
+    },
+    authenticated: false,
+  }),
+
+  getters: {
+    is_authenticated() {
+      // Check if the is_authenticated key exists in localStorage
+      return localStorage.is_authenticated || this.authenticated;
+    },
+    getFavoriteSongs() {
+      return this.user.favorite_songs;
+    },
+  },
+
+  actions: {
+    setUserData(name, surname, code) {
+      this.user.name = name;
+      this.user.surname = surname;
+      this.user.code = code;
+    },
+    authenticate(email, password) {
+      // Check if the provided email and password match your credentials
+      if (email === 'your-email@example.com' && password === '123456') {
+        // Set the is_authenticated key in localStorage to true
+        localStorage.is_authenticated = true;
+        // Set the authenticated variable to true
+        this.authenticated = true;
+        // Programatically change the router address to / (Home)
+        router.push('/');
+      }
+    },
+    logout() {
+      // Clear localStorage when logging out
+      localStorage.clear();
+      // Set authenticated to false
+      this.authenticated = false;
+      // Programatically change the router address to /login
+      router.push('/login');
+    },
+    toggleFavorite(songID) {
+      // Check if the songID is already in the favorite_songs array
+      const index = this.user.favorite_songs.indexOf(songID);
+      if (index === -1) {
+        // If not, add it to the favorite_songs array
+        this.user.favorite_songs.push(songID);
+      } else {
+        // If it is, remove it from the favorite_songs array
+        this.user.favorite_songs.splice(index, 1);
+      }
+
+      // Store the updated favorite_songs array in localStorage
+      localStorage.favorite_songs = JSON.stringify(this.user.favorite_songs);
+    },
+  },
+});

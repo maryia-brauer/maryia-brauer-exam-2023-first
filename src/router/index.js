51c1153c122e6e81ffd6c6889df400bf2324d7a6
Maryia <maryia.brauer@gmail.com>
2023-11-03 13:58:20 +0200

    Done tasks 7-12

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import { useAuthStore } from '@/stores/auth.js'; 
-import Login from '@/views/Login.vue'
-
+import Login from '@/views/Login.vue';
+import Songs from '@/views/Songs.vue';
+import Albums from '@/views/Albums.vue';
+import About from '@/views/About.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes : [
         {
-            path: '/login',
-            component: Login, 
-          },
+          path: '/login',
+          component: Login, 
+        },
+        {
+          path: '/',
+          component: Songs, 
+        },
+        {
+          path: '/albums',
+          component: Albums, 
+        },
+        {
+          path: '/about',
+          component: About, 
+        },
     ]
 })
 
